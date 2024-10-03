0a351f5250415a601dd4cd0dc3ce3f382ca23876
unknown <akhepar@asu.edu>
2024-10-03 09:25:27 -0700

    idea for managing state list

--- app/src/main/java/com/akheparasu/tic_tac_toe/screens/GameScreen.kt
+++ app/src/main/java/com/akheparasu/tic_tac_toe/screens/GameScreen.kt
@@ -1,11 +1,15 @@
 package com.akheparasu.tic_tac_toe.screens
 
+import android.util.Log
 import androidx.compose.foundation.Canvas
+import androidx.compose.foundation.LocalIndication
 import androidx.compose.foundation.clickable
+import androidx.compose.foundation.indication
 import androidx.compose.foundation.layout.*
-import androidx.compose.foundation.text.BasicTextField
+import androidx.compose.runtime.saveable.Saver
 import androidx.compose.material3.*
 import androidx.compose.runtime.*
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
@@ -18,9 +22,28 @@ import kotlinx.coroutines.delay
 @Composable
 fun GameScreen() {
     val settings = LocalSettings.current
-    val gridSize = settings.gridSizeFlow.collectAsState(initial = DEFAULT_GRID_SIZE).value
-    var grid by remember { mutableStateOf(Array(gridSize) { Array(gridSize) { "" } }) }
-    var playerTurn by remember { mutableStateOf(true) }
+    val gridSizeFlow = settings.gridSizeFlow.collectAsState(initial = null)
+    val difficultyFlow = settings.difficultyFlow.collectAsState(initial = null)
+    if (gridSizeFlow.value==null || difficultyFlow.value==null) {
+        return Box(
+            modifier = Modifier.fillMaxSize(),
+            contentAlignment = Alignment.Center
+        ) {
+            CircularProgressIndicator()
+        }
+    }
+    val gridSize by rememberSaveable { mutableIntStateOf(gridSizeFlow.value!!) }
+    // Define your gridSaver as before
+//    val gridSaver = Saver<List<List<String>>, ArrayList<ArrayList<String>>>(
+//        save = { grid ->
+//            ArrayList(grid.map { ArrayList(it) })
+//        },
+//        restore = { saved: ArrayList<ArrayList<String>> ->
+//            saved.map { it.toList() }
+//        }
+//    )
+    var grid by rememberSaveable { mutableStateOf(Array(gridSize) { Array(gridSize) { "" } }) }
+    var playerTurn by rememberSaveable { mutableStateOf(true) }
     val isGameComplete: (Array<Array<String>>) -> Boolean = { !it.any { c -> c.any { v -> v.isEmpty() } } }
 
     LaunchedEffect(playerTurn) {
@@ -31,6 +54,12 @@ fun GameScreen() {
             } else {
                 playerTurn = true
             }
+        }
+    }
+    LaunchedEffect (gridSize) {
+        if (gridSize!=grid.size) {
+            grid = Array(gridSize) { Array(gridSize) { "" } }
+            playerTurn = true
         }
     }
 
@@ -49,8 +78,12 @@ fun GameScreen() {
             Row {
                 for (j in 0 until gridSize) {
                     GridCell(value = grid[i][j], onTap = {
-                        if (playerTurn && grid[i][j] == "") {
-                            grid[i][j] = "X"
+                        if (playerTurn && grid[i][j].isEmpty()) {
+//                            grid = grid.toMutableList().apply {
+//                                this[i] = this[i].toMutableList().apply {
+//                                    this[j] = 'X'
+//                                }
+//                            }
                             if (isGameComplete(grid)) {
                                 // finishGame()
                             } else {
@@ -75,18 +108,15 @@ fun GridCell(value: String, onTap: () -> Unit) {
     Box(
         modifier = Modifier
             .size(100.dp)
-            .padding(8.dp),
+            .padding(8.dp)
+            //.indication(interactionSource, LocalIndication.current),
+            .clickable { onTap() },
         contentAlignment = Alignment.Center
     ) {
         Canvas(modifier = Modifier.fillMaxSize()) {
             drawRect(color = Color.LightGray)
         }
-        BasicTextField(
-            value = value,
-            onValueChange = { },
-            readOnly = true,
-            modifier = Modifier.clickable(onClick = onTap)
-        )
+        Text(text = value.toString())
     }
 }
 
