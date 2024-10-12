155ff81f354a5c23732d53090354fb4fb569afe8
krgilman <145879807+krgilman@users.noreply.github.com>
2024-10-11 21:59:09 -0700

    Added MiniMax.kt and updated GameScreen.kt
    The minimax.kt has 3 functions, runAITurn which is a controller and does the easy difficulty return. It then calls the miniMax function for the medium and hard difficulties, which runs the minimax algorithm with alpha-beta pruning. The final function is needed to figure out when someone wins so that the miniMax algorithm is able to know how to assign values. The depth is there to cause earlier wins to be prioritized over later ones. The game code still needs to be updated to display a win and pass the difficulty to the controller.

---
+++ screens/HomeScreen.kt
@@ -0,0 +1,47 @@
+package com.akheparasu.tic_tac_toe.screens
+
+import android.os.Bundle
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Button
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.akheparasu.tic_tac_toe.utils.LocalNavController
+
+
+@Composable
+fun HomeScreen() {
+    val navController = LocalNavController.current
+    Column(
+        modifier = Modifier
+            .fillMaxSize()
+            .padding(16.dp),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ) {
+        Button(onClick = {
+
+            navController?.navigate("game/${false}") }){
+            Text(text = "Play against Computer")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { navController?.navigate("game/${true}") }) {
+            Text(text = "Play against Player")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { /* Placeholder for online multiplayer */ }) {
+            Text(text = "Play Online (Coming Soon)")
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = { navController?.navigate("career") }) {
+            Text(text = "Career")
+        }
+    }
+}
